9df3c27d02cdcb0ad78525ab219c0815b12b8695
benoly <benoly@gmail.com>
2016-11-22 14:27:08 -0700

    added put

--- public/components/items/item.js
+++ public/components/items/item.js
@@ -13,18 +13,106 @@ OneItem = React.createClass({
         });
     },
 
+    putItem: function (){
+    var id = this.props.id;
+    var updatedItem = {
+      name: this.state.name,
+      description: this.state.description,
+      estimatedLabor: this.state.estimatedLabor,
+      estimatedCost: this.state.estimatedCost,
+      dateCompleted: this.state.dateCompleted,
+      image: this.state.image
+    };
+      $.ajax ({
+        method: "PUT",
+        url: "/item/" + id,
+        data: updatedItem,
+        success: function(response){
+          this.props.getItems();
+        }.bind(this),
+        error: function(xhr, status, err){
+          console.error(status, err.toString());
+        }.bind(this)
+    });
+  },
+
+  changeName: function(event){
+  this.setState({
+    name: event.target.value
+  })
+},
+  changeDescription: function(event){
+  this.setState({
+    description: event.target.value
+  })
+},
+  changeEstimatedLabor: function(event){
+  this.setState({
+    estimatedLabor: event.target.value
+  })
+},
+  changeEstimagedCost: function(event){
+  this.setState({
+    estimatedCost: event.target.value
+  })
+},
+  changeDateCompleted: function(event){
+  this.setState({
+    dateCompleted: event.target.value
+  })
+},
+  changeImage: function(event){
+  this.setState({
+    image: event.target.value
+  })
+},
+
+updateItem: function(){
+    this.setState({
+      view:
+        <div key ={this.props.id}>
+          <form onSubmit = {this.putItem}>
+              <input placeholder = {this.props.name} onChange = {this.changeName}/>
+              <input placeholder = {this.props.description} onChange = {this.changeDescription}/>
+              <input placeholder = {this.props.estimatedLabor} onChange = {this.changeEstimatedLabor}/>
+              <input placeholder = {this.props.estimatedCost} onChange = {this.changeEstimatedCost}/>
+              <input placeholder = {this.props.dateCompleted} onChange = {this.changeDateCompleted}/>
+              <input placeholder = {this.props.image} onChange = {this.changeImage}/>
+              <img src = {this.state.image}/>
+              <button type = "submit">Submit Change</button>
+          </form>
+        </div>
+    })
+  },
+
+  getInitialState: function(){
+    return {
+      view:
+      <div key={this.props.id}>
+          <h1>{this.props.name}</h1>
+          <p>{this.props.user}</p>
+          <p>{this.props.dateCreated}</p>
+          <p>{this.props.description}</p>
+          <p>{this.props.estimatedLabor}</p>
+          <p>{this.props.estimatedCost}</p>
+          <p>{this.props.dateCompleted}</p>
+          <img src={this.props.image}/>
+          <button onClick={this.deleteItem}>DELETE This Item</button>
+          <button onClick={this.updateItem}>UPDATE This Item</button>
+      </div>,
+      name: this.props.name,
+      description: this.props.description,
+      estimatedLabor: this.props.estimatedLabor,
+      estimatedCost: this.props.estimatedCost,
+      dateCompleted: this.props.dateCompleted,
+      image: this.props.image
+    }
+  },
+
     render: function(){
         return (
-            <div key={this.props.id}>
-                <h1>{this.props.name}</h1>
-                <p>{this.props.user}</p>
-                <p>{this.props.dateCreated}</p>
-                <p>{this.props.description}</p>
-                <p>{this.props.estimatedLabor}</p>
-                <p>{this.props.estimatedCost}</p>
-                <p>{this.props.dateCompleted}</p>
-                <img src={this.props.image}/>
-                <button onClick={this.deleteItem}>DELETE This Item</button>
+            <div>
+              {this.state.view}
             </div>
         );
     }
