73b94f3a8ff148b293a4c0cf9eb397732739d3a2
tressacroaker <tressacroaker36@gmail.com>
2016-11-29 15:57:50 -0700

    proving to kyle we got this

--- public/components/home/homePage.js
+++ public/components/home/homePage.js
@@ -2,7 +2,7 @@ HomePage = React.createClass({
   render: function(){
     return(
       <div>
-        <h1>Hey there fancy pants!</h1>
+        <h1>Hey there not so fancy pants!</h1>
       </div>
     );
   }
