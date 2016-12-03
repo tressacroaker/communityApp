723e23cef9dc83c146280d19a046cf5d1302b3a0
benoly <benoly@gmail.com>
2016-12-03 16:16:02 -0700

    added home pic and three arrow

--- public/components/home/homePage.js
+++ public/components/home/homePage.js
@@ -2,8 +2,18 @@ HomePage = React.createClass({
   render: function(){
     return(
       <div className = "wrapper">
-        <h1>Hey there not so fancy pants!</h1>
-        <img className="hands" src="hands.jpg"/>
+        <div className = "homePicBox">
+          <img className="hands" src="buckets.jpg"/>
+          <div className = "tagline">
+              <p id="line1">Community Development.</p>
+              <p id="line2">One Team at a Time.</p>
+          </div>
+          <button>Help Fund a Project</button>
+        </div>
+        <div className="threeArrowsBox">
+          <h2 id="howItWorks">How Team Fundable Works</h2>
+          <img className="threeArrowsPic" src="threearrows.jpg"/>
+        </div>
       </div>
     );
   }
