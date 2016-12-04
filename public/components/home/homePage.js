a39aa8d5afca81e217fdbd313b02bef5c5e4e853
benoly <benoly@gmail.com>
2016-12-04 16:33:49 -0700

    more homepage stylin, homey

--- public/components/home/homePage.js
+++ public/components/home/homePage.js
@@ -10,9 +10,28 @@ HomePage = React.createClass({
           </div>
           <button>Help Fund a Project</button>
         </div>
-        <div className="threeArrowsBox">
-          <h2 id="howItWorks">How Team Fundable Works</h2>
-          <img className="threeArrowsPic" src="threearrows.jpg"/>
+          {/* <h2 id="howItWorks">How Team Fundable Works</h2> */}
+          <img className="threeArrowsPic" src="twoArrows.png"/>
+        <div className="tripleBox">
+          <div className="leftTripleBox">
+            <p id="tripleBoxTitle"> Community </p>
+            <p>Local organization needs help completing a project </p>
+            <p>Past projects include beautifying public spaces, planting trees, and volunteering at homeless shelters.</p>
+            <p>Organization posts description of project with Team Fundable</p>
+            <p>Team volunteers their services</p>
+            <p>You make it happen by funding the match!</p>
+          </div>
+          <div className="middleTripleBox">
+            <p id="tripleBoxTitle"> You </p>
+          </div>
+          <div className="rightTripleBox">
+            <p id="tripleBoxTitle"> Teams </p>
+            <p>want to raise money for an upcoming event </p>
+            <p>This could include Little League trips out of town, school band buying instruments, or Boy Scouts needing new uniforms.</p>
+            <p>Teams post the description of their event and what it will take to reach this goal.</p>
+            <p>Community Oragnizations are matched with the Teams</p>
+            <p>You make it happen by funding the match!</p>
+          </div>
         </div>
       </div>
     );
